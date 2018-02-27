c1dc2b290d5c7500b3aad42f2f481d6abbb05755
Hercílio <hercilio.ortiz@acad.pucrs.br>
2018-02-26 23:30:49 -0300

    Xml e adapter da memoria episodica completa

--- app/src/main/java/com/example/hercilio/appwithfirebase/Funcionalidades/Bateria/MemoriaEpisodica/GridActivity.java
+++ app/src/main/java/com/example/hercilio/appwithfirebase/Funcionalidades/Bateria/MemoriaEpisodica/GridActivity.java
@@ -1,5 +1,7 @@
 package com.example.hercilio.appwithfirebase.Funcionalidades.Bateria.MemoriaEpisodica;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.hercilio.appwithfirebase.R;
 
@@ -23,11 +26,13 @@ public class GridActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memoria_eposodica_segunda_fase);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view_grid_mem_ep);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        GridView gridview = (GridView) findViewById(R.id.gridview_mem_ep);
+        gridview.setAdapter(new GridAdapter(this));
 
-        recyclerView.setAdapter(new GridAdapter());
-        recyclerView.setHasFixedSize(true);
+//        recyclerView.setAdapter(new GridAdapter());
+//        recyclerView.setHasFixedSize(true);
 //        recyclerView.addItemDecoration(new GridDecoration(this));
     }
+
+
 }
