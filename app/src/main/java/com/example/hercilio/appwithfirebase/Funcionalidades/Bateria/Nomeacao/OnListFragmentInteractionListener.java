dbab3b68d9d4f7672b1e6c7cfbd9b88cfa9c449e
Hercílio <hercilio.ortiz@acad.pucrs.br>
2018-03-05 14:13:19 -0300

    Revert "Revert "Images for nomeacao ""
    This reverts commit b0bc3a58940905ec4fb6ef6c897840d4a9d11049.

---
+++ app/src/main/java/com/example/hercilio/appwithfirebase/Funcionalidades/Bateria/Nomeacao/OnListFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package com.example.hercilio.appwithfirebase.Funcionalidades.Bateria.Nomeacao;
+
+import android.widget.ImageButton;
+
+/**
+ * Created by Hercilio on 26/12/2017.
+ */
+
+interface OnListFragmentInteractionListener {
+    void onListFragmentInteraction(ImageButton item, boolean ref);
+}
